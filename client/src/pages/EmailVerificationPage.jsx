8cbd50a7df75ad2bc49fe901dccdbf50ce757416
Erin Muzughi <52017685+erinmuzughi@users.noreply.github.com>
2024-02-09 14:05:20 -0600

    setup front end pages

---
+++ client/src/pages/EmailVerificationPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import EmailVerificationComponent from '../components/EmailVerificationComponent';
+
+export default function  EmailVerificationPage() {
+  return (
+    <div>
+        <EmailVerificationComponent />
+    </div>
+  )
+}
